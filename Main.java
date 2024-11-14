85f69e99a74435b68244cd05302eff264601750f
Rosintan Sitinjak <2381014@unai.edu>
2024-11-14 10:06:51 +0700

    Handson9

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+//import repositories.TodoListRepository;
+//import repositories.TodoListRepositorylmpl;
+//import services.TodoListService;
+//import services.TodoListServicelmpl;
+//import views.TodoListTerminalView;
+//import views.TodoListView;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        TodoListRepository todoListRepository = new TodoListRepositorylmpl();
+//        TodoListService todoListService = new TodoListServicelmpl(todoListRepository);
+//        TodoListView todoListView = new TodoListTerminalView(todoListService);
+//        todoListView.run();
+//    }
+//}
+//
+
+import config.Database;
 import repositories.TodoListRepository;
-import repositories.TodoListRepositorylmpl;
+import repositories.TodoListRepositoryDblmpl;
 import services.TodoListService;
 import services.TodoListServicelmpl;
 import views.TodoListTerminalView;
@@ -7,10 +25,13 @@ import views.TodoListView;
 
 public class Main {
     public static void main(String[] args) {
-        TodoListRepository todoListRepository = new TodoListRepositorylmpl();
-        TodoListService todoListService = new TodoListServicelmpl(todoListRepository);
+
+        Database database = new Database("my_database", "root", "", "localhost", "3306");
+        database.setup();
+
+        TodoListRepository todoListRepository = new TodoListRepositoryDblmpl(database);
+        TodoListService todoListService =new TodoListServicelmpl(todoListRepository);
         TodoListView todoListView = new TodoListTerminalView(todoListService);
         todoListView.run();
     }
 }
-
